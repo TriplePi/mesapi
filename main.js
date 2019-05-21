0573e460264e621d838b7245722d419ab8ca6f83
Sandbox Sync <support@getsandbox.com>
2019-05-21 12:49:21 +0000

    Changes

--- main.js
+++ main.js
@@ -58,4 +58,4 @@ Sandbox.define("/api/workcenter", "POST", api.postApiWorkcenter);
 Sandbox.define("/api/workcenter/{workCenterId}", "DELETE", api.deleteApiWorkcenter);
 Sandbox.define("/api/workcenter/{workCenterId}/service", "POST", api.postApiWorkcenterService);
 Sandbox.define("/api/workcenter/{workCenterId}/workingtimes", "POST", api.postApiWorkcenterWorkingtimes);
-Sandbox.define('/api/signin','GET', api.postSignIn);
+Sandbox.define('/api/signin','POST', api.postSignIn);
