e54aeb8d844a0dbec80135f90fc4d546010db222
Sandbox Sync <support@getsandbox.com>
2019-06-09 06:11:37 +0000

    Changes

--- routes/api.js
+++ routes/api.js
@@ -779,7 +779,7 @@ exports.getApiWorkcenterStatus = function (req, res) {
     res.type('json');
     res.json(_.find(workcenters, {
         'Id': Number(req.params.workCenterId)
-    }));
+    }).push(statuses[randomInteger(0, statuses.length - 1)]));
 };
 
 function randomInteger(min, max) {
